9d7dda6c00743287ba638749a2bc6ba104bb35b4
Martin Kouba <mkouba@redhat.com>
2018-06-28 09:10:56 +0200

    Misc cleanup work
    - remove snapshot dependencies
    - ignore FaultToleranceTest for now
    - update readme

--- testsuite/basic/src/test/java/io/smallrye/restclient/tests/ft/FaultToleranceTest.java
+++ testsuite/basic/src/test/java/io/smallrye/restclient/tests/ft/FaultToleranceTest.java
@@ -42,6 +42,7 @@ import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -54,6 +55,7 @@ import io.smallrye.restclient.app.Timer;
  *
  * @author Martin Kouba
  */
+@Ignore("We need SmallRye MP FT implementation")
 @RunWith(Arquillian.class)
 public class FaultToleranceTest {
 
